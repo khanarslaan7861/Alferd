7b8104a2a4cda4a8887843240b56ef5444459a68
khanarslaan7861 <khanarslaan7861@gmail.com>
2023-03-25 10:13:49 +0530

    new file:   alferd.py 	new file:   alferd_model.model.h5 	new file:   classes.pkl 	new file:   ext/index.html 	new file:   intents.json 	new file:   static/bg2.png 	new file:   static/chat.css 	new file:   static/chat.js 	new file:   static/header-bg.png 	new file:   static/hotel1.jpg 	new file:   static/img1.png 	new file:   static/img3.png 	new file:   static/img4.png 	new file:   static/img5.png 	new file:   static/img6.png 	new file:   static/menu-btn.png 	new file:   static/style.css 	new file:   static/touristspot.png 	new file:   static/tours.jpg 	new file:   templates/index.html 	new file:   training.py 	new file:   words.pkl

---
+++ alferd.py
@@ -0,0 +1,75 @@
+import json
+import pickle
+import random
+import nltk
+import numpy as np
+from nltk.stem import WordNetLemmatizer
+from tensorflow.python.keras.models import load_model
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('alferd_model.model.h5')
+
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    error_threshold = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > error_threshold]
+    results.sort(key=lambda x: x[1], reverse=True)
+    result_list = []
+    for r in results:
+        result_list.append({'intents': classes[r[0]], 'probability': str([r[1]])})
+    return result_list
+
+
+def get_responses(intents_list, intents_json):
+    tag = intents_list[0]['intents']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+
+print('Bot is running')
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    ints = predict_class(userText)
+    res = get_responses(ints, intents)
+    return str(res)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
