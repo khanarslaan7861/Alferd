237de8f6d69c96c0b461fc4ab90c247714f76c96
khanarslaan7861 <khanarslaan7861@gmail.com>
2023-08-15 17:27:26 +0530

    alferd.py: 1: Added and changed tensorflow logging to '2' 2: Changed flask host to local only

--- alferd.py
+++ alferd.py
@@ -1,3 +1,5 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import json
 import pickle
 import random
@@ -73,4 +75,4 @@ def get_bot_response():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run()
